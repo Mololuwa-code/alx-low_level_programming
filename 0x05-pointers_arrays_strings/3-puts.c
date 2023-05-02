f27e53576617a680f0712973ab408a07d3ec228a
Mololuwa-code <Mololuwafamewo@gmail.com>
2023-05-02 15:11:27 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: Pointer to a string.
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
