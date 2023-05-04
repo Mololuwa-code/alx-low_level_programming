f056b0326d2c85cdb46c8846a45ac303a84933cc
Mololuwa-code <Mololuwafamewo@gmail.com>
2023-05-04 11:08:45 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - copies a string, up to n characters
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of characters to copy
+ *
+ * Return: pointer to destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
