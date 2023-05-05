4421dd438a43b8e1101be9423e8c07813b86b095
Mololuwa-code <Mololuwafamewo@gmail.com>
2023-05-04 18:06:07 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13 encryption
+ *
+ * @s: Pointer to a string to be encoded
+ *
+ * Return: Pointer to the encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
