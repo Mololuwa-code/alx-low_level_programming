9202ceb99ef2b144cd691ffa17333086f087fde6
Mololuwa-code <Mololuwafamewo@gmail.com>
2023-05-16 14:20:33 -0700

    Fix trailing white space and add new files

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,34 +2,34 @@
 #include "main.h"
 
 /**
- * _sqrt_helper - helper function for _sqrt_recursion
+ * _sqrt - helper function that returns the square root of n
  * @n: the number to find the square root of
- * @x: the guess for the square root
+ * @guess: the current guess for the square root
  *
- * Return: the natural square root of n, or -1 if n does not have a
- *         natural square root
+ * Return: the square root of n, or -1 if n does not have a natural square root
  */
-int _sqrt_helper(int n, int x)
+int _sqrt(int n, int guess)
 {
-	if (x * x > n)
-		return (-1);
-	if (x * x == n)
-		return (x);
-	return (_sqrt_helper(n, x + 1));
+    /* base case */
+    if (guess * guess == n)
+        return guess;
+
+    /* if the guess squared is greater than n, there's no natural square root */
+    if (guess * guess > n)
+        return -1;
+
+    /* try the next guess */
+    return _sqrt(n, guess + 1);
 }
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: the number to find the square root of
  *
- * Return: the natural square root of n, or -1 if n does not have a
- *         natural square root
+ * Return: the square root of n, or -1 if n does not have a natural square root
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	return (_sqrt_helper(n, 1));
+    /* start with a guess of 1 */
+    return _sqrt(n, 1);
 }
---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Number of command-line arguments
+ * @argv: Array containing the command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf(my name is : "%s\n", argv[0]);
+	return (0);
+}
